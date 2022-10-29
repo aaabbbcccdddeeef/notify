11335516a6009269d2f6cb71a218da59f3fef093
shadowprompt <huixiong.cn@gmail.com>
2022-10-29 20:56:42 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -8921,11 +8921,7 @@ async function run() {
       body,
     }).then((response) => {
       const data = response.data || {};
-      if (data.code === '200') {
-        console.log('bark push 成功 -> ');
-      } else {
-        console.log('bark push 不成功 -> ');
-      }
+      console.log('bark push result -> ', data);
     }).catch(err => {
       console.log('bark push 失败 -> ', err);
     })
